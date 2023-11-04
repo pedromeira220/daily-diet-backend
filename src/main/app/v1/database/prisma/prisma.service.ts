eee076830411195d513164b2217e71fac2f78813
Pedro Meira <pedrohmeira@icloud.com>
2023-11-04 15:06:47 -0300

    chore: adding npx before nest in npm scripts

--- src/main/app/v1/database/prisma/prisma.service.ts
+++ src/main/app/v1/database/prisma/prisma.service.ts
@@ -4,6 +4,8 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
+    console.log('> database url env', process.env.DATABASE_URL);
+
     super({
       log: process.env.NODE_ENV != 'test' ? ['query'] : [],
     });
